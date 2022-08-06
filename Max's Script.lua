621a05e49f3bbd6c4363c11dfb480866f3da77c5
MaxDragonBoi <109011094+MaxDragonBoi@users.noreply.github.com>
2022-08-06 12:11:27 -0400

    Update Max's Script.lua

--- Max's Script.lua
+++ Max's Script.lua
@@ -1,7 +1,7 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("maxs First script hub", "BloodTheme")
 
-
+if game.PlaceId == 5194438573 then
     -- MAIN
     local main = Window:NewTab("main")
     local MainSection = main:NewSection("Main")
@@ -21,14 +21,6 @@ local Window = Library.CreateLib("maxs First script hub", "BloodTheme")
         end
     end)
     
-    --PET SIM X
-    local PET SIM X = Window:NewTab("PET SIM X")
-    local PET SIM XSection = main:NewSection("PET SIM X")
-
-    PET SIM XSection:NewButton("Pet Sim X", "Pet Sim X hack", function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Wind596/RaitoHub.c-/main/RaitoHub", true))()'))()
-    end)
-
 
     MainSection:NewButton("Infinite Yield", "FE Admin Commands", function()
         loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
@@ -66,3 +58,67 @@ local Window = Library.CreateLib("maxs First script hub", "BloodTheme")
 
         Fly(true)
     end)
+
+--WEAPON FIGHTING GAME SCRIPT
+elseif game.PlaceId == 8554378337 then
+    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+    local Window = Library.CreateLib("weapon hub", "BloodTheme")
+     
+    -- MAIN
+     local Main = Window:NewTab("Main")
+     local MainSection = Main:NewSection("Main")
+ 
+ 
+     MainSection:NewButton("Back/Front Flip", "Makes you do gymnastics", function()
+         loadstring(game:HttpGet('https://pastebin.com/raw/7wDcPtLk'))()
+     end)
+ 
+     MainSection:NewToggle("Super-Human", "go fast and jump high", function(state)
+         if state then
+             game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 120
+             game.Players.LocalPlayer.Character.Humanoid.JumpPower = 120
+         else
+             game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+             game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
+         end
+     end)
+
+
+     
+     MainSection:NewButton("Infinite Yield", "FE Admin Commands", function()
+         loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+     end)
+ 
+ 
+     --LOCAL PLAYER
+     local Player = Window:NewTab("Player")
+     local PlayerSection = Player:NewSection("Player")
+ 
+     PlayerSection:NewSlider("Walkspeed", "SPEED!!", 500, 16, function(s)
+         game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+     end)
+ 
+     PlayerSection:NewSlider("Jumppower", "JUMP HIGH!!", 350, 50, function(s)
+         game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+     end)
+ 
+     PlayerSection:NewButton("Reset WS/JP", "Resets to all defaults", function()
+         game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
+         game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+     end)
+ 
+ 
+     --Other
+     local Other = Window:NewTab("Other")
+     local OtherSection = Other:NewSection("Other")
+ 
+     OtherSection:NewButton("Chat Spoofer", "Lets you chat for other people", function()
+         loadstring(game:HttpGet(('https://pastebin.com/raw/djBfk8Li'),true))()
+     end)
+ 
+     OtherSection:NewButton("Bypassed Fly", "bird mode", function()
+         loadstring(game:HttpGet("https://raw.githubusercontent.com/Nicuse/RobloxScripts/main/BypassedFly.lua"))() 
+ 
+         Fly(true)
+     end)
+ end
